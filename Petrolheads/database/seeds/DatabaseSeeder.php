e11c58d2c5a23e97825170525d36ce8f193cd086
Ivanov,Dimitar D.A <dimitar.ivanov@student.fontys.nl>
2019-11-25 19:45:27 +0100

    Seed

--- Petrolheads/database/seeds/DatabaseSeeder.php
+++ Petrolheads/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('posts')->insert([
+            'title' => 'Car',
+            'content' => 'Super cool car',
+        ]);
+        DB::table('posts')->insert([
+            'title' => 'Another Car',
+            'content' => 'Another Super cool car',
+        ]);
     }
 }
