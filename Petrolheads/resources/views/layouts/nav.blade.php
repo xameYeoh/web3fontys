998954e654da02b62615669910421d0d0b49ea47
Kirill <kgetmanskii@gmail.com>
2019-11-25 21:30:08 +0100

    links

--- Petrolheads/resources/views/layouts/nav.blade.php
+++ Petrolheads/resources/views/layouts/nav.blade.php
@@ -8,19 +8,17 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
+                    <a class="nav-link" href="welcome">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
+                    <a class="nav-link" href="overview">Overview</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
+
             </ul>
         </div>
     </div>
