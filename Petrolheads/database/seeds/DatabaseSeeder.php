740bf04a311827a69538c61e46a96065f11595dc
Martin <Grigorov>
2019-11-26 15:18:56 +0200

    Week 2

--- Petrolheads/database/seeds/DatabaseSeeder.php
+++ Petrolheads/database/seeds/DatabaseSeeder.php
@@ -20,5 +20,15 @@ class DatabaseSeeder extends Seeder
             'title' => 'Another Car',
             'content' => 'Another Super cool car',
         ]);
+        DB::table('comments')->insert([
+            'user_id' => '12',
+            'post_id' => '1',
+            'content' => 'Dear Car Talk: I have a 1951 Chevy, straight-6, 3-on-the-tree, with 31,000 actual miles. Nice car.',
+        ]);
+        DB::table('comments')->insert([
+            'user_id' => '9',
+            'post_id' => '1',
+            'content' => 'Nice car!',
+        ]);
     }
 }
