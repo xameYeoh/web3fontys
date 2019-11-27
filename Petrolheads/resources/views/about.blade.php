45cc058b67067d9981fafb7597ff0d4b507c0c00
Ivanov,Dimitar D.A <dimitar.ivanov@student.fontys.nl>
2019-11-27 11:40:34 +0100

    Design

--- Petrolheads/resources/views/about.blade.php
+++ Petrolheads/resources/views/about.blade.php
@@ -5,10 +5,24 @@
 @endsection
 
 @section('content')
+<div class="container">
 
-    <div class="title m-b-md">
-        About us
-    </div>
+    <div class="row">
+<!-- Post Content Column -->
+<div class="col-lg-8">
+
+    <!-- Title -->
+    <h1 class="mt-4">@yield('title', 'Post')</h1>
+
+
+    <hr>
+
+    <!-- Date/Time -->
+    <p>@yield('time', 'Yesterday')</p>
+
+    <hr>
+
+    
     <div>
     <li>Martin Grigorov</li>
     <li>Kirill Getmanskii</li>
--- Petrolheads/resources/views/posts/index.blade.php
+++ Petrolheads/resources/views/posts/index.blade.php
@@ -7,17 +7,41 @@
 @section('content')
 
 
+<div class="container">
 
+    <div class="row">
+<!-- Post Content Column -->
+<div class="col-lg-8">
+
+    <!-- Title -->
+    <h1 class="mt-4">@yield('title', 'Post')</h1>
+
+
+    <hr>
+
+    <!-- Date/Time -->
+    <p>@yield('time', 'Yesterday')</p>
+
+    <hr>
+
+    <!-- Preview Image -->
+    <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+
+    <hr>
+
+    
+    <blockquote class="blockquote">
+        <p class="mb-0">@yield('content')</p>
+        
+    </blockquote>
+
+    <hr>
     <div>
         @foreach($posts as $post)
             <li>{{$post->title}}</li>
             <p>{{$post->content}}</p>
         @endforeach
-    </div>
-
-@endsection
-
-@foreach($comments as $comment)
+        @foreach($comments as $comment)
     @section('commentator')
         {{$comment->user_id}}
     @endsection
@@ -27,3 +51,22 @@
     @endsection
 
 @endforeach
+    </div>
+    @include('layouts.comform')
+
+    @include('layouts.comment')
+
+    @include('layouts.nescomment')
+
+</div>
+
+
+       
+
+    </div>
+    <!-- /.row -->
+
+</div>
+@endsection
+
+
