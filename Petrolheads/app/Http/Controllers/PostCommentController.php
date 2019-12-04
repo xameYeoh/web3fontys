02e3955c5c91f2644d622e4332494b607e55e238
Martin <Grigorov>
2019-12-04 14:21:59 +0200

    Foreign keys

--- Petrolheads/app/Http/Controllers/PostCommentController.php
+++ Petrolheads/app/Http/Controllers/PostCommentController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Post;
 use \App\Comment;
+use \App\User;
+
 
 class PostCommentController extends Controller
 {
-    public function store(Post $post)
+    public function store(Post $post, User $user)
     {
         Comment::create([
-            'user_id' =>$post->id,
+            'user_id' =>$user->id,
             'post_id' => $post->id,
             'content' => request('content')
         ]);
