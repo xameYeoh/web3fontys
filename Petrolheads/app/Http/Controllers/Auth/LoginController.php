004ab59543ff6e7b632f5819c11a81de8120435b
Kirill <kgetmanskii@gmail.com>
2020-01-12 22:37:16 +0100

    fix logout

--- Petrolheads/app/Http/Controllers/Auth/LoginController.php
+++ Petrolheads/app/Http/Controllers/Auth/LoginController.php
@@ -52,15 +52,5 @@ class LoginController extends Controller
 
         return $this->sendFailedLoginResponse($request);
     }
-    public function logout(Request $request)
-    {
-        $user = Auth::guard('api')->user();
 
-        if ($user) {
-            $user->api_token = null;
-            $user->save();
-        }
-
-        return response()->json(['data' => 'User logged out.'], 200);
-    }
 }
