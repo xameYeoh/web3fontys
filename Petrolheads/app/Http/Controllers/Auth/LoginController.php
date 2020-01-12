11438dfcf0d4f8377a24ec96c2d630584bbe48a7
Kirill <kgetmanskii@gmail.com>
2020-01-12 23:19:42 +0100

    fix

--- Petrolheads/app/Http/Controllers/Auth/LoginController.php
+++ Petrolheads/app/Http/Controllers/Auth/LoginController.php
@@ -37,20 +37,20 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    public function login(Request $request)
-    {
-        $this->validateLogin($request);
-
-        if ($this->attemptLogin($request)) {
-            $user = $this->guard()->user();
-            $user->generateToken();
-
-            return response()->json([
-                'data' => $user->toArray(),
-            ]);
-        }
-
-        return $this->sendFailedLoginResponse($request);
-    }
+//    public function login(Request $request)
+//    {
+//        $this->validateLogin($request);
+//
+//        if ($this->attemptLogin($request)) {
+//            $user = $this->guard()->user();
+//            $user->generateToken();
+//
+//            return response()->json([
+//                'data' => $user->toArray(),
+//            ]);
+//        }
+//
+//        return $this->sendFailedLoginResponse($request);
+//    }
 
 }
